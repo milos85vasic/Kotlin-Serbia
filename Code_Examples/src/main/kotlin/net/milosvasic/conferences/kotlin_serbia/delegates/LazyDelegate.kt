903bad2289abf1c92eab13f7a1084cee1f9c0c02
Milos Vasic <milos85vasic@gmail.com>
2017-03-18 16:05:28 +0100

    Kotlin Serbia: Lazy initialization.

--- Code_Examples/src/main/kotlin/net/milosvasic/conferences/kotlin_serbia/delegates/LazyDelegate.kt
+++ Code_Examples/src/main/kotlin/net/milosvasic/conferences/kotlin_serbia/delegates/LazyDelegate.kt
@@ -1,5 +1,28 @@
 package net.milosvasic.conferences.kotlin_serbia.delegates
 
-fun main(args: Array<String>){
+/**
+ * Class that uses lazy initialization to initialize it's field.
+ */
+class PostgreClient {
 
+    /**
+     * Lazy initialization.
+     */
+    val database by lazy { // by using 'lazy' delegate
+        Database("PostgreSQL")
+    }
+
+}
+
+class Database(val type: String) {
+    init {
+        println("Initializing $type")
+    }
+}
+
+fun main(args: Array<String>) {
+    val client = PostgreClient()
+    val database = client.database // Initialization will be fired here
+    val database2 = client.database // And only once
+    val database3 = client.database // ...
 }
