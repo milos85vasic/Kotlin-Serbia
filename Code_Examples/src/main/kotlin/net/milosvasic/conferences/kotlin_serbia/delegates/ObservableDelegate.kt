3a9acd1f73d3bdac8599077f65dd491a3ed7025e
Milos Vasic <milos85vasic@gmail.com>
2017-03-18 16:14:59 +0100

    Kotlin Serbia: Observable.

---
+++ Code_Examples/src/main/kotlin/net/milosvasic/conferences/kotlin_serbia/delegates/ObservableDelegate.kt
@@ -0,0 +1,21 @@
+package net.milosvasic.conferences.kotlin_serbia.delegates
+
+import kotlin.properties.Delegates
+
+/**
+ * Stock value with observer delegate
+ */
+class Stock {
+
+    var value : Int by Delegates.observable(0) { // Initial value is 0
+        property, old, new -> println("${property.name}: [ $old ] -> [ $new ]")
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val stock = Stock()
+    stock.value = 10   // Outputs: value: [ 0 ] -> [ 10 ]
+    stock.value = 100  // Outputs: value: [ 10 ] -> [ 100 ]
+    stock.value = 1000 // Outputs: value: [ 100 ] -> [ 1000 ]
+}
